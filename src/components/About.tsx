2bd8660c4b18c6cc8265098eac1ba4370c730f3b
jwhite88 <74882417+jwhite88@users.noreply.github.com>
2022-08-29 14:30:22 -0400

    Update Projecst component and adde gh-pages

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,8 +49,8 @@ function About() {
             <div className='flex flex-row justify-around text-left'>
                 <section className='w-1/3 releative ' >
                     <div id='aboutOne' ref={aboutOne} >
-                        <h1 className='t text-5xl italic pt-5 pb-4' style={{ fontFamily: "Arima Madurai",textShadow: "1px 1px rgba(255,255,255,0.5)" }}>Passion...</h1>
-                        <p className='leading-9 tracking-wide text-3xl ' id="para" style={{ fontFamily: "Laila", textShadow: "1px 1px rgba(255,255,255,0.5)", }}>
+                        <h1 className='t text-5xl italic pt-5 pb-4' style={{ fontFamily: "Arima Madurai",textShadow: "1px 1px rgba(255,255,255,0.3)" }}>Passion...</h1>
+                        <p className='leading-9 tracking-wide text-3xl ' id="para" style={{ fontFamily: "Laila", textShadow: "1px 1px rgba(255,255,255,0.3)", }}>
                             Passion ipsum dolor sit amet consectetur adipisicing elit. Earum cum
                             aspernatur corrupti labore alias perspiciatis architecto nemo
                             delectus amet ea vero praesentium reiciendis, omnis repellat eius,
@@ -62,9 +62,9 @@ function About() {
                 </section>
                 <section id='mypg' className='w-1/3  releative ' >
                     <div id='aboutTwo' ref={aboutTwo}>
-                        <h1 className='t text-5xl italic pt-5 pb-4' style={{ fontFamily: "Arima Madurai", textShadow: "1px 1px rgba(255,255,255,0.5)" }}>Understading...</h1>
+                        <h1 className='t text-5xl italic pt-5 pb-4' style={{ fontFamily: "Arima Madurai", textShadow: "1px 1px rgba(255,255,255,0.3)" }}>Understading...</h1>
                         <p className='leading-9 tracking-wide text-3xl ' id="paratwo" style={{
-                            fontFamily: "Laila", textShadow: "1px 1px rgba(255,255,255,0.5)", }}>
+                            fontFamily: "Laila", textShadow: "1px 1px rgba(255,255,255,0.3)", }}>
                             Understanding ipsum dolor sit amet consectetur adipisicing elit. Dolorum non
                             voluptatem nihil est, ullam dolor nobis id vero quo excepturi
                             voluptas architecto, amet numquam itaque.
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 function Projects() {
-    return <div id='projects' className='h-[100vh] pt-[20px] text-offblack-500 bg-tinygreen-900'>Projects</div>;
+    return <div id='projects' className='h-[100vh] pt-[20px] text-offblack-500 bg-tinygreen-900'>
+        <h3 className="py-4 text-6xl">Projects</h3>
+        <div className="project-container">
+            <div className="project-flex flex flex-row flex-wrap justify-between w-screen px-6">
+                <div className="project-card-container">
+                    <div className="project-card rounded-lg bg-whitemarble-300 w-96 min-h-[5rem] pt-2 px-3 text-left border-l-offblack-300">
+                        <h4 className="project-name">Project Name</h4>
+                        <div className="project-img"></div>
+                        <div className="project-url"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
 }
 
 export default Projects
