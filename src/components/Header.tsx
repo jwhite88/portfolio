d5344e2748f02c118d28a15f44c980feb20fd2d9
jwhite88 <74882417+jwhite88@users.noreply.github.com>
2022-08-30 13:36:02 -0400

    Added logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Scrollchor } from "react-scrollchor";
+import jw from '../assets/images/jw-name.png'
 
 function Header() {
     return (
@@ -8,8 +9,10 @@ function Header() {
             style={{ fontFamily: "Roboto Mono", fontWeight: 400 }}
             className='flex flex-row justify-between items-center min-h-[36px] px-10 text-offblack-500 bg-tinygreen-700'
         >
-            <div className='logo'>logo</div>
-            <nav className='flex flex-row flex-nowrap justify-between  basis-96 '>
+            <div className='logo'>
+                <img className="h-[100px]" src={jw} alt="John White" />
+            </div>
+            <nav className='flex flex-row flex-nowrap justify-between  basis-[32rem] text-2xl '>
                 <div className='link'>
                     <Scrollchor to='#about'>About</Scrollchor>
                 </div>
