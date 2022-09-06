418e66fc00e169e5aaef2e1e90e586ca482e88f1
jwhite88 <74882417+jwhite88@users.noreply.github.com>
2022-09-06 09:03:33 -0400

    Added images to second project and updated CSS on Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import project1 from '../assets/images/project1.png'
+import project2 from '../assets/images/project2bc.png'
 
 function Projects() {
     return <div id='projects' className='h-[100vh] pt-[20px] text-offblack-500 bg-tinygreen-900'>
-        <h3 className="py-4 text-6xl">Projects</h3>
-        <div className="project-container">
-            <div className="project-flex flex flex-row flex-wrap justify-between w-screen px-6">
+        <h3 className="py-6 text-6xl pb-8">Projects</h3>
+        <div className="project-container w-screen flex flex-row  justify-around">
+            <div className="project-flex flex flex-row flex-wrap justify-between px-6">
                 <div className="project-card-container">
                     <div className="project-card rounded-lg bg-whitemarble-300 w-96 min-h-[5rem] pt-2 px-3 hover:scale-105 ease-out duration-300 text-left border-l-offblack-300">
                         <h4 className="project-name">Our Planet</h4>
@@ -14,6 +15,19 @@ function Projects() {
                         </div>
                         <div className="project-url">
                             <a href="https://jwhite88.github.io/naturegreen/">Visit Site</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="project-flex flex flex-row flex-wrap justify-between px-6">
+                <div className="project-card-container">
+                    <div className="project-card rounded-lg bg-whitemarble-300 w-96 min-h-[5rem] pt-2 px-3 hover:scale-105 ease-out duration-300 text-left border-l-offblack-300">
+                        <h4 className="project-name">Weather Application</h4>
+                        <div className="project-img">
+                            <img className='aspect-auto object-cover p-3' src={project2} alt="project1" />
+                        </div>
+                        <div className="project-url">
+                            <a href="https://jwhite88.github.io/weatherapiproj/">Visit Site</a>
                         </div>
                     </div>
                 </div>
