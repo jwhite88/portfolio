2ed39feb4d9b5564e70cbf96c7d521f6b96e1962
jwhite88 <74882417+jwhite88@users.noreply.github.com>
2022-10-24 13:12:53 -0400

    Added another project and updated project with mapping

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,36 +2,32 @@ import React from 'react'
 import project1 from '../assets/images/project1.png'
 import project2 from '../assets/images/project2bc.png'
 
+import { projects } from "../data/projects";
+
 function Projects() {
-    return <div id='projects' className='h-[100vh] pt-[20px] text-offblack-500 bg-tinygreen-900'>
+    return <div id='projects' className='h-[100%] pt-[20px] pb-[100px] text-offblack-500 bg-tinygreen-900'>
         <h3 className="py-6 text-6xl pb-8">Projects</h3>
-        <div className="project-container w-screen flex flex-row  justify-around">
-            <div className="project-flex flex flex-row flex-wrap justify-between px-6">
-                <div className="project-card-container">
-                    <div className="project-card rounded-lg bg-whitemarble-300 w-96 min-h-[5rem] pt-2 px-3 hover:scale-105 ease-out duration-300 text-left border-l-offblack-300">
-                        <h4 className="project-name">Our Planet</h4>
-                        <div className="project-img">
-                            <img className='aspect-auto object-cover p-3' src={project1} alt="project1" />
+        <div className="project-container w-screen flex flex-wrap  justify-around">
+            {projects.map((project) => {
+                return (
+                    <>
+                        <div className="project-flex flex flex-row flex-wrap justify-between px-6">
+                            <div className="project-card-container">
+                                <div className="project-card rounded-lg bg-whitemarble-300 w-96 min-h-[5rem] pt-2 px-3 hover:scale-105 ease-out duration-300 text-left border-l-offblack-300">
+                                    <h4 className="project-name">{project.name}</h4>
+                                    <div className="project-img">
+                                        <img className='aspect-auto object-cover p-3' src={require(`../assets/images/${project.image}.png`)} alt="project1" />
+                                    </div>
+                                    <div className="project-url">
+                                        <a href={project.demoLink}>Live Demo</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="project-url">
-                            <a href="https://jwhite88.github.io/naturegreen/">Visit Site</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="project-flex flex flex-row flex-wrap justify-between px-6">
-                <div className="project-card-container">
-                    <div className="project-card rounded-lg bg-whitemarble-300 w-96 min-h-[5rem] pt-2 px-3 hover:scale-105 ease-out duration-300 text-left border-l-offblack-300">
-                        <h4 className="project-name">Weather Application</h4>
-                        <div className="project-img">
-                            <img className='aspect-auto object-cover p-3' src={project2} alt="project1" />
-                        </div>
-                        <div className="project-url">
-                            <a href="https://jwhite88.github.io/weatherapiproj/">Visit Site</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    </>
+                )
+            })
+            }
         </div>
     </div>;
 }
---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    name: "Our Planet",
+    image: "project1",
+    demoLink: "https://jwhite88.github.io/naturegreen/",
+    githubLink: "",
+  },
+  {
+    name: "Weather Application",
+    image: "project2bc",
+    demoLink: "https://jwhite88.github.io/weatherapiproj/",
+    githubLink: "",
+  },
+  {
+    name: "Meals Site",
+    image: "project3",
+    demoLink: "https://jwhite88.github.io/meals-site",
+    githubLink: "",
+  },
+];
